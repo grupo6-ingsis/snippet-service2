703097b211aba877d1550b2e4c694e7b08b32cc0
Agustín Guglielmelli <agusguglielmelli@gmail.com>
2025-09-30 20:05:52 -0300

    initial commit

---
+++ src/main/kotlin/org/gudelker/snippet_service/SnippetServiceApplication.kt
@@ -0,0 +1,11 @@
+package org.gudelker.snippet_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SnippetServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<SnippetServiceApplication>(*args)
+}
---
+++ src/test/kotlin/org/gudelker/snippet_service/SnippetServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.gudelker.snippet_service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SnippetServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
