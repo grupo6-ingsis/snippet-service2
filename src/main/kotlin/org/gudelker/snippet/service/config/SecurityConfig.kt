8ed7d8ed64fda36ca3ca9991e7eba99ec385a408
pedrodelaguila <pdelaguila@mail.austral.edu.ar>
2025-10-23 14:52:22 -0300

    Changed config

--- src/main/kotlin/org/gudelker/snippet/service/config/SecurityConfig.kt
+++ src/main/kotlin/org/gudelker/snippet/service/config/SecurityConfig.kt
@@ -1,19 +1,26 @@
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.Customizer.withDefaults
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
 
+@Configuration
 @EnableWebSecurity
 class SecurityConfig {
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
-        http
-            .csrf { it.disable() }
-            .authorizeHttpRequests { auth ->
-                auth.anyRequest().authenticated()
+        http.authorizeHttpRequests {
+            it
+                .anyRequest().authenticated()
+        }
+            .oauth2ResourceServer { it.jwt(withDefaults()) }
+            .cors {
+                it.disable()
             }
-            .oauth2ResourceServer { oauth2 ->
-                oauth2.jwt { }
+            .csrf {
+                it.disable()
             }
         return http.build()
     }
