d40392151e0839bff5edc707790c291e90c4a5e9
pedrodelaguila <pdelaguila@mail.austral.edu.ar>
2025-11-16 02:38:08 -0300

    feat: languages #8 with language request param

--- src/main/kotlin/org/gudelker/snippet/service/modules/language/LanguageController.kt
+++ src/main/kotlin/org/gudelker/snippet/service/modules/language/LanguageController.kt
@@ -1,5 +1,6 @@
 package org.gudelker.snippet.service.modules.language
 
+import org.gudelker.snippet.service.modules.language.dto.LanguageWithExtensionDto
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -10,8 +11,13 @@ class LanguageController(
     private val languageRepository: LanguageRepository,
 ) {
     @GetMapping("/supported")
-    fun getSupportedLanguages(): List<String> {
+    fun getSupportedLanguages(): List<LanguageWithExtensionDto> {
         val languages = languageRepository.findAll()
-        return languages.map { it.name }
+        return languages.map {
+            LanguageWithExtensionDto(
+                language = it.name,
+                extension = it.extension,
+            )
+        }
     }
 }
