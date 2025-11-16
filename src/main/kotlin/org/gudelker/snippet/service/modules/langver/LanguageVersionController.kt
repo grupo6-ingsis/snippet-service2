4b902d7af142dfe13e3f14bc7a73cd02f3c5881b
pedrodelaguila <pdelaguila@mail.austral.edu.ar>
2025-11-16 00:40:10 -0300

    feat: languages #5 with languageVersion

---
+++ src/main/kotlin/org/gudelker/snippet/service/modules/langver/LanguageVersionController.kt
@@ -0,0 +1,29 @@
+package org.gudelker.snippet.service.modules.langver
+
+import org.gudelker.snippet.service.modules.language.LanguageRepository
+import org.gudelker.snippet.service.modules.langver.dto.LanguageVersionDto
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/language-version")
+class LanguageVersionController(
+    private val languageVersionRepository: LanguageVersionRepository,
+    private val languageRepository: LanguageRepository,
+) {
+    @GetMapping("/supported")
+    fun getSupportedLanguageVersions(languageName: String): List<LanguageVersionDto> {
+        val language =
+            languageRepository.findByName(languageName)
+                ?: return emptyList()
+        val versions = languageVersionRepository.findByLanguageName(languageName)
+        return LanguageVersionDto(
+            languageName = languageName,
+            versions = versions.map { it.version },
+            extension = language.extension,
+        ).let {
+            listOf(it)
+        }
+    }
+}
