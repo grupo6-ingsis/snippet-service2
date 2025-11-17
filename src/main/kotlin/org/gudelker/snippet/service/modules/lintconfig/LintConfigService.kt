27e31a6a16569e3892a9e7195e78b09859d66234
pedrodelaguila <pdelaguila@mail.austral.edu.ar>
2025-11-17 03:36:46 -0300

    activate / deactivate lint rules

--- src/main/kotlin/org/gudelker/snippet/service/modules/lintconfig/LintConfigService.kt
+++ src/main/kotlin/org/gudelker/snippet/service/modules/lintconfig/LintConfigService.kt
@@ -1,9 +1,42 @@
 package org.gudelker.snippet.service.modules.lintconfig
 
+import org.gudelker.snippet.service.modules.lintconfig.input.ActivateRuleRequest
+import org.gudelker.snippet.service.modules.lintrule.LintRuleRepository
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
+import org.springframework.web.server.ResponseStatusException
+import java.util.UUID
 
 @Service
-class LintConfigService(private val lintConfigRepository: LintConfigRepository) {
+class LintConfigService(
+    private val lintConfigRepository: LintConfigRepository,
+    private val lintRuleRepository: LintRuleRepository,
+) {
+    fun activateRule(
+        request: ActivateRuleRequest,
+        userId: String,
+    ): LintConfig {
+        val ruleId = UUID.fromString(request.id)
+        val rule = lintRuleRepository.findById(ruleId).orElseThrow()
+        val config =
+            LintConfig().apply {
+                this.userId = userId
+                this.lintRule = rule
+                this.ruleValue = request.ruleValue
+            }
+        return lintConfigRepository.save(config)
+    }
+
+    fun deactivateRule(
+        userId: String,
+        lintRuleId: UUID,
+    ) {
+        val config =
+            lintConfigRepository.findByUserIdAndLintRuleId(userId, lintRuleId)
+                ?: throw ResponseStatusException(HttpStatus.NOT_FOUND)
+        lintConfigRepository.delete(config)
+    }
+
     fun getAllRulesFromUser(userId: String): List<LintConfig> {
         return lintConfigRepository.findByUserId(userId)
     }
---
+++ src/main/kotlin/org/gudelker/snippet/service/modules/lintconfig/input/ActivateRuleRequest.kt
@@ -0,0 +1,8 @@
+package org.gudelker.snippet.service.modules.lintconfig.input
+
+data class ActivateRuleRequest(
+    val id: String,
+    val name: String,
+    val isActive: Boolean,
+    val ruleValue: String,
+)
