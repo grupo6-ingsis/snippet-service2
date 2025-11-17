a79b756c18e0869f254fad4721457605950d05be
pedrodelaguila <pdelaguila@mail.austral.edu.ar>
2025-11-17 02:34:41 -0300

    get all lint rules from user

--- src/main/kotlin/org/gudelker/snippet/service/modules/lintconfig/LintConfigController.kt
+++ src/main/kotlin/org/gudelker/snippet/service/modules/lintconfig/LintConfigController.kt
@@ -3,18 +3,16 @@ package org.gudelker.snippet.service.modules.lintconfig
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/lintconfig")
 class LintConfigController(private val lintConfigService: LintConfigService) {
-    @GetMapping("/{userId}")
+    @GetMapping("/user")
     fun getLintConfigsByUserId(
-        @PathVariable userId: String,
         @AuthenticationPrincipal jwt: Jwt,
     ): List<LintConfig> {
-        return lintConfigService.getAllRulesFromUser(userId)
+        return lintConfigService.getAllRulesFromUser(jwt.subject)
     }
 }
