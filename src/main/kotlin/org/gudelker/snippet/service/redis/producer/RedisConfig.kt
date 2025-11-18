607889a48873913b43251d3913f394560ff8bf36
pedrodelaguila <pdelaguila@mail.austral.edu.ar>
2025-11-18 14:56:36 -0300

    feat: object mapper config

--- src/main/kotlin/org/gudelker/snippet/service/redis/producer/RedisConfig.kt
+++ src/main/kotlin/org/gudelker/snippet/service/redis/producer/RedisConfig.kt
@@ -1,6 +1,8 @@
 package org.gudelker.snippet.service.redis.producer
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -12,7 +14,10 @@ import org.springframework.data.redis.serializer.StringRedisSerializer
 class RedisConfig {
     @Bean
     fun objectMapper(): ObjectMapper {
-        return ObjectMapper().registerKotlinModule()
+        return ObjectMapper()
+            .registerKotlinModule()
+            .registerModule(JavaTimeModule())
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
     }
 
     @Bean
