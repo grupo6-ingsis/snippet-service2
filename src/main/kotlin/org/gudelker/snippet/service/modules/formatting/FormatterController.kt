5d7067b09ff83da463c6793d7608fe99a32ebc53
pedrodelaguila <pdelaguila@mail.austral.edu.ar>
2025-11-21 02:01:08 -0300

    feat: format snippet

--- src/main/kotlin/org/gudelker/snippet/service/modules/formatting/FormatterController.kt
+++ src/main/kotlin/org/gudelker/snippet/service/modules/formatting/FormatterController.kt
@@ -1,9 +1,9 @@
 package org.gudelker.snippet.service.modules.formatting
 
+import org.gudelker.snippet.service.modules.formatting.formatconfig.input.FormatSingleSnippetRequest
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.security.oauth2.jwt.Jwt
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -12,12 +12,12 @@ import java.util.UUID
 @RestController
 @RequestMapping("/format")
 class FormatterController(private val formattingOrchestratorService: FormattingOrchestratorService) {
-    @PostMapping("/snippet/{snippetId}")
+    @PostMapping("/snippet")
     fun formatSnippet(
-        @PathVariable snippetId: String,
+        input: FormatSingleSnippetRequest,
         @AuthenticationPrincipal jwt: Jwt,
     ): ResponseEntity<String> {
-        val id = UUID.fromString(snippetId)
+        val id = UUID.fromString(input.snippetId)
         val result = formattingOrchestratorService.formatSingleSnippet(id, jwt.subject)
         return ResponseEntity.ok(result)
     }
---
+++ src/main/kotlin/org/gudelker/snippet/service/modules/formatting/formatconfig/input/FormatSingleSnippetRequest.kt
@@ -0,0 +1,6 @@
+package org.gudelker.snippet.service.modules.formatting.formatconfig.input
+
+data class FormatSingleSnippetRequest(
+    val snippetId: String,
+    val content: String,
+)
